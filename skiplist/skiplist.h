512dfe049476f4c34b299b9975d5f2c21efe2f79
854324563y <854324563@qq.com>
2023-04-15 22:49:07 +0800

    add move semantics

--- skiplist/skiplist.h
+++ skiplist/skiplist.h
@@ -22,6 +22,24 @@ private:
             forward = new skip_list_node * [level + 1];
             for (int i = 0; i <= level; ++i) forward[i] = nxt;  //初始化，指向next节点
         }
+        //移动构造函数
+        skip_list_node(skip_list_node&& other) noexcept
+            //其中key为int、forward为指针数组，没有'所有权'，不需使用std::move
+            : key(std::move(other.key)),value(std::move(other.value)),level(other.level),forward(other.forward) {
+            other.forward = nullptr;
+        }
+        //移动赋值运算符
+        skip_list_node& operator = (skip_list_node&& other) noexcept {
+            if (this != &other) {
+                delete [] forward;
+                key = std::move(other.key);
+                value = std::move(other.value);
+                level = other.level;
+                forward = other.forward;
+                other.forward = nullptr;
+            }
+            return *this;
+        }
         ~skip_list_node() { delete[] forward; }
 //        ~skip_list_node() {
 //            for (int i=0; i<=level; ++i) delete forward[i];
@@ -107,6 +125,26 @@ public:
         }
         ++length;                                   //节点个数+1
     }
+    void insert(K&& key, V&& value) {
+        node *update[MAXL + 1];
+        node* p = find(key,update);
+        if (p->key == key) {
+            p->value = std::move(value);
+            return;
+        }
+        int lv = randomLevel();
+        if (lv > level) {
+            lv = ++level;
+            update[lv] = head;
+        }
+        node * newNode = new node(std::move(key), std::move(value), lv);
+        for (int i = lv; i >= 0; --i) {
+            p = update[i];
+            newNode->forward[i] = p->forward[i];
+            p->forward[i] = newNode;
+        }
+        ++length;
+    }
 
     bool erase(const K& key) {
         node* update[MAXL + 1];
@@ -140,55 +178,3 @@ public:
         return Iter(head->forward[0]);
     }
 };
-int main()
-{
-    {
-        //使用lambda
-        auto cmp = [](const string& a, const string& b) {return a.length() < b.length(); };
-        skip_list < string, int, decltype(cmp)> list(cmp);
-        list.insert("aab", 1321);
-        list.insert("hello", 54342);
-        list.insert("world", 544);
-        for (auto it = list.begin(); it != list.end(); it++) {
-            cout << it->key << " " << it->value << endl;
-        }
-    }
-
-    cout << "==================================" << endl;
-
-    {
-        //使用仿函数
-        struct cmp {
-            bool operator()(int a, int b) {
-                return a > b;
-            }
-        };
-        skip_list < int, int, cmp> list{};
-        for (int i = 1; i <= 10; i++)list.insert(rand()%20, rand());
-        for (auto it = list.find(10); it != list.end(); it++) {
-            cout << it->key << " " << it->value << endl;
-        }
-    }
-
-    cout << "==================================" << endl;
-
-    {
-        //默认小于号
-        skip_list<int, int>list;
-        list.insert(1, 3);
-        list.insert(1, 3);
-        list.insert(4, 3);
-        list.insert(5, 3);
-        list.insert(1, 3);
-        list.insert(4, 3);
-        for (auto it = list.begin(); it != list.end();it++) {
-            cout << it->key << " " << it->value << endl;
-        }
-
-    }
-
-    {
-        //可以添加 T && 实现move语义
-    }
-
-}
